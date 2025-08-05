b638e445cca78a0e186d2b57778f19d8d752ba08
Vedant Kumar <vedantvisoliya@gmail.com>
2025-08-05 17:23:35 +0530

    filing changed

--- Email-Slicer-Program(easy)/main.py
+++
@@ -1,9 +0,0 @@
-# python email slicer
-
-email_address = input("Enter your email: ")
-index = email_address.find("@")
-
-username = email_address[:index]
-domain = email_address[index+1:]
-
-print(f"Your username is {username},\n& your domain is {domain}")
